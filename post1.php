c8c03d3addcf387e5005cbd3abd688ce38a36926
Dinda Nurafni <90329718+DindaNurafniUPI@users.noreply.github.com>
2021-11-04 19:40:12 +0700

    Add files via upload

---
+++ post1.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>SIK UPIS</title>
+    <style>
+    footer{
+	background: indigo;
+	color: white;
+  padding: 14px 16px;
+  width: 100%;
+}
+</style>
+</head>
+<php?
+?>
+<body>
+    
+<body data-new-gr-c-s-check-loaded="14.1036.0" data-gr-ext-installed="" action="post.php" method="post">
+    
+<header>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+<div class="w3-container w3-black">
+<img src="https://psikologi.upi.edu/wp-content/uploads/2021/06/Untitled-1-01-e1624637650473.png" class="bi me-2" width="300" height="90" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+    <a href="utama.php" class="w3-bar-item w3-button w3-round w3-white ">Home</a>
+    <div class="w3-dropdown-hover">
+      <button class="w3-button  ">Profil</button>
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="post2.php" class="w3-bar-item w3-button">Sejarah</a>
+        <a href="post3.php" class="w3-bar-item w3-button">Visi Misi</a>
+        <a href="post4.php" class="w3-bar-item w3-button">Struktur Organisasi</a>
+        <a href="post1.php" class="w3-bar-item w3-button">Dosen</a>
+      </div>
+    </div>
+    <div class="w3-dropdown-hover">
+      <button class="w3-button  ">Akademik</button>
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="post5.php" class="w3-bar-item w3-button">kurikulum</a>
+        <a href="post6.php" class="w3-bar-item w3-button">Laboratorium</a>
+      </div>
+    </div>
+    <a href="post7.php" class="w3-bar-item w3-button">Akreditasi</a>
+    <div class="w3-dropdown-hover">
+      <button class="w3-button ">Download</button>
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="https://drive.google.com/file/d/1QzVIjXXNbZxCYD2rnOdaMWR_7k83amyw/view" class="w3-bar-item w3-button">SK Syarat Peringat Akreditasi</a>
+        <a href="https://drive.google.com/file/d/1CijlD1g5NZH2Jkwvf90TXoUYX3rdcYp9/view" class="w3-bar-item w3-button">Struktur Kurikulum</a>
+        <a href="https://drive.google.com/drive/folders/12tzM1qZQ9NG99olZvmA_syL-TH44oXh2" class="w3-bar-item w3-button">Rencana pembelajaran studi (RPS)</a>
+        <a href="https://drive.google.com/drive/folders/1ZlC8ZQWGvVBEEoRyfmFhJ83XofXxtgrY?usp=sharing" class="w3-bar-item w3-button">Nilai Perkuliahan</a>
+      </div>
+    </div>
+    <a href="post8.php" class="w3-bar-item w3-button ">FAQ</a>
+    <a href="post9.php" class="w3-bar-item w3-button ">HIMATASKA</a><br>
+  
+
+</header>
+<center>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+<img src="sik.jpg" alt="Chicago" class="d-block" style="width:100%">
+<br>
+<p class="w3-serif w3-red w3-center h2"> DOSEN SISTEM INFORMASI KELAUTAN</P>
+<div class="w3-container">
+ 
+  <div class="w3-panel w3-leftbar w3-blue">
+    <p class="w3-large w3-serif"><i>"Dosen Program Studi Sistem Informasi Kelautan (SIK) Universitas Pendidikan Indonesia (UPI) terdiri dari dosen-dosen muda yang professional di bidang Ilmu Kelautan, Perikanan, Sistem Informasi Geografis, Pengindraan Jauh, Oseanografi, Konservasi, Sistem Informasi, Biologi Laut, Ekologi Laut. Sosial Ekonomi, Biosistematika, Mitigasi Bencana, dan bidang lainnya. Dosen Program Studi SIK merupakan lulusan dari universitas ternama seperti, Universitas Indonesia (UI), Institut Teknologi Bandung (ITB), Institut Pertanian Bogor (IPB).
+Program studi Sistem Informasi Kelautan juga didukung dengan dosen – dosen yang berkualitas dan berpengalaman dari UPI Kampus Serang dalam bidang Bahasa Inggris, Statistika, Matematika dan sebagainya. Perkuliahan juga melibatkan kerjasama dengan stakeholder yang terkait serta praktisi dalam bidang system informasi dan kelautan."</i></p>
+    <p class="w3-serif">SISTEM INFORMASI KELAUTAN</p>
+  </div>
+</div>
+
+    <div class="row">
+      <div class="col-lg-4">
+        <img src="bunovi.jpg" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
+
+        <h2 class="w3-serif">NOVI SOFIA FITRIASARI</h2>
+        <h5 class="w3-serif">Lektor</h5>
+        <p class="w3-serif">S.Si. - Universitas Padjadjaran (2007)<br>
+M.T. - Institut Teknologi Bandung (2010)<br>
+NIP. 197811042010122001 </p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="buayang.png" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
+<br>
+        <h2 class="w3-serif">AYANG ARMELITA ROSALIA</h2>
+        <h5 class="w3-serif"> Tenaga Pengajar</h5>
+        <p class="w3-serif">S.Pi. - Institut Pertanian Bogor (2016)<br>
+M.Si. - Institut Pertanian Bogor (2018)<br>
+NIPTT. 020190219941209201
+
+   </p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="budella.png" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
+
+        <h2 class="w3-serif">DELLA AYU LESTARI</h2>
+        <h5 class="w3-serif"> Tenaga Pengajar </h5>
+        <p class="w3-serif"> S.Si. - Universitas Indonesia (2017)<br>
+M.Si. - Universitas Indonesia (2018)<br>
+NIPTT. 020190219950428201</p>
+
+</p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="buanzani.png" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
+
+
+        <h2 class="w3-serif">LUTHFI ANZANI</h2>
+        <h5 class="w3-serif">Tenaga Pengajar</h5>
+        <p class="w3-serif">S.Pd. - Universitas Negeri Jakarta (2014)<br>
+M.Si. - Institut Pertanian Bogor (2018)<br>
+NIPTT. 020190219911202201
+
+ </p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="pawildann.png" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
+
+<br>
+        <h2 class="w3-serif">WILLDAN APRIZAL ARIFIN</h2>
+        <h5 class="w3-serif">Tenaga Pengajar</h5>
+        <p class="w3-serif">S.Pd. - Universitas Pendidikan Indonesia (2016)<br>
+M.Kom. - Institut Pertanian Bogor (2019)<br>
+NIPT. 920200819940415101 </p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="paishak.png" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
+
+        <h2 class="w3-center w3-serif">ISHAK ARIAWAN</h2>
+        <h5 class="w3-serif">Tenaga Pengajar</h5>
+        <p class="w3-serif"> S.Pd. - Universitas Negeri Makasar (2015)<br>
+M.Kom. - Institut Pertanian Bogor (2019)<br>
+NIPT. 920200819940117102</p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="paalam.png" class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
+
+        <h2 class="w3-serif">LA ODE ALAM MINSARIS</h2>
+        <h5 class="w3-serif">Tenaga Pengajar</h5>
+        <p class="w3-serif">S.Pi. - Universitas Halu Oleo (2015)<br>
+M.Si. - Institut Pertanian Bogor (2019)<br>
+NIPT. 920200819900404101 </p>
+      </div><!-- /.col-lg-4 -->
+    </div>
+    
+<main><meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+.mySlides {display:none;}
+</style>
+<body>
+
+<div class="w3-content" style="max-width:2000px">
+  <div class="mySlides w3-container w3-red">
+    <h1><b>Did You Know?</b></h1>
+    <h1><i>with our lecture</i></h1>
+  </div>
+
+  <div class="w3-content" style="max-width:2000px">
+  <div class="mySlides w3-container w3-blue">
+    <h1><b>Did You Know?</b></h1>
+    <h1><i>with our lecture</i></h1>
+  </div>
+
+</div>
+
+<script>
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 1000); 
+}
+</script>
+  </div></div>
+
+<br>
+
+
+      <hr>
+<h1 class="w3-serif w3-red">INFO KAMI</h1>
+    <div class="container px-7 py-5 " id="featured-3">
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <div class="feature-icon bg-primary bg-gradient">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
+        </div>
+        <h2>Hubungi kami</h2>
+          <a href="tel:(0254) 200277">Telepon : (0254) 200277</a><br>
+          <a href="mailto:sik_kdserang@upi.edu">Email :sik_kdserang@upi.edu </a><br>
+       
+      </div>
+      <div class="feature col">
+        <div class="feature-icon bg-primary bg-gradient">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg>
+        </div>
+        <h2>Tautan</h2>
+        <a href="http://kd-purwakarta.upi.edu/">UPI PURWAKARTA</a></li><br>
+        <a href="http://kd-serang.upi.edu/" >UPI SERANG</a></li><br>
+        <a href="http://kd-sumedang.upi.edu/" >UPI SUMEDANG</a></li><br>
+        <a href="http://kd-tasikmalaya.upi.edu/" >UPI TASIKMALAYA</a></li><br>
+        <a href="https://kd-cibiru.upi.edu/">UPI CIBIRU</a></li>
+         </div>
+      <div class="feature col">
+        <div class="feature-icon bg-primary bg-gradient">
+          <svg class="bi" width="2em" height="1em"><use xlink:href="#toggles2"></use></svg>
+        </div>
+        <div class="col-4 offset-1">
+        <form>
+      <!-- start sw-rss-feed code --> 
+<script type="text/javascript" > 
+<!-- 
+rssfeed_url = new Array(); 
+rssfeed_url[0]="https://rss.app/feeds/mXOkAF4QmA70wmds.xml";  
+rssfeed_frame_width="300"; 
+rssfeed_frame_height="210"; 
+rssfeed_scroll="on"; 
+rssfeed_scroll_step="6"; 
+rssfeed_scroll_bar="off"; 
+rssfeed_target="_blank"; 
+rssfeed_font_size="12"; 
+rssfeed_font_face=""; 
+rssfeed_border="on"; 
+rssfeed_css_url="https://feed.surfing-waves.com/css/style6.css"; 
+rssfeed_title="on"; 
+rssfeed_title_name=""; 
+rssfeed_title_bgcolor="#3366ff"; 
+rssfeed_title_color="#fff"; 
+rssfeed_title_bgimage=""; 
+rssfeed_footer="off"; 
+rssfeed_footer_name="rss feed"; 
+rssfeed_footer_bgcolor="#fff"; 
+rssfeed_footer_color="#333"; 
+rssfeed_footer_bgimage=""; 
+rssfeed_item_title_length="50"; 
+rssfeed_item_title_color="#666"; 
+rssfeed_item_bgcolor="#fff"; 
+rssfeed_item_bgimage=""; 
+rssfeed_item_border_bottom="on"; 
+rssfeed_item_source_icon="off"; 
+rssfeed_item_date="off"; 
+rssfeed_item_description="on"; 
+rssfeed_item_description_length="120"; 
+rssfeed_item_description_color="#666"; 
+rssfeed_item_description_link_color="#333"; 
+rssfeed_item_description_tag="off"; 
+rssfeed_no_items="0"; 
+rssfeed_cache = "66bd4ace5056cbd9be7ebf215bb8d9b4"; 
+//--> 
+</script> 
+<script type="text/javascript" src="//feed.surfing-waves.com/js/rss-feed.js"></script> 
+<!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. --> 
+<div style="color:#ccc;font-size:10px; text-align:right; width:230px;">powered by <a href="https://surfing-waves.com" rel="noopener" target="_blank" style="color:#ccc;">Surfing Waves</a></div> 
+<!-- end sw-rss-feed code -->
+        </form>
+        
+      </div>
+    </div>  
+    <hr>
+
+<div class="d-flex justify-content-between py-4 my-4 border-top">
+  <p>© 2021 SIK UPI, Inc. All rights reserved.</p>
+</div>
+      </div>
+    </div>
+  </div>
+
+  </footer>
+</main>
+
+
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+      
+  
+
+</body>
+</div>
+</body>
+</html>
